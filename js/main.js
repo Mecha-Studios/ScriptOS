de4aebee4d031fea998f31278bc2cddaac1e8bd4
TenZe Inc <33192847+TenZeInc@users.noreply.github.com>
2018-08-20 15:45:35 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -251,6 +251,42 @@ function dragElement(elmnt) {
 
 }
 
+dragElement(document.getElementById(("scriptstore")));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+
+
+}
+
 var input = document.getElementById('input');
 var form = document.getElementById('commands');
 
@@ -306,7 +342,7 @@ function start(invar, outvar) {
         outvar("2. https://www.mariospizzaonline.com")
     }
     if (invar == "Clear") {
-        output.removeChild(newPara)
+        output.removeChild(outtext)
     }
     if (invar == "Who is Cortana?") {
         outvar("Cortana is a virtual assistant created by Microsoft Inc.")
