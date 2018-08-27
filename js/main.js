6ecb968f2f92787b33b9534200013710b65b2ca3
TenZe Inc <33192847+TenZeInc@users.noreply.github.com>
2018-08-27 16:32:42 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -287,6 +287,42 @@ function dragElement(elmnt) {
 
 }
 
+dragElement(document.getElementById(("textapp")));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+
+
+}
+
 var input = document.getElementById('input');
 var form = document.getElementById('commands');
 
@@ -321,31 +357,84 @@ function start(invar, outvar) {
     if (invar == "") {
         outvar("Sorry I didn't get that")
     }
-    if (invar == "Hi") {
+    if (invar == "hi") {
         outvar("Hello")
     }
-    if (invar == "How are you?") {
+    if (invar == "how are you") {
         outvar("I'm doing pretty good, you?")
     }
-    if (invar == "What time is it?") {
+    if (invar == "what time is it") {
         outvar("I'm sorry, I cannot currently tell time. That will be coming in a future update.")
     }
-    if (invar == "Do you have voice control?") {
-        outvar("I do not currently have voice recognition, but it is currently being worked on!")
+    if (invar == "do you have voice controll") {
+        outvar("Yes!")
     }
-    if (invar == "What is your name?") {
+    if (invar == "whats your name") {
         outvar("My name is Zec. I am your digital assistant.")
     }
-    if (invar == "Order a pizza") {
+    if (invar == "order a pizza") {
         outvar("Here are some pizza places:");
         outvar("1. https://www.dominos.com")
         outvar("2. https://www.mariospizzaonline.com")
     }
-    if (invar == "Clear") {
-        output.removeChild(outtext)
-    }
-    if (invar == "Who is Cortana?") {
+    if (invar == "who is Cortana") {
         outvar("Cortana is a virtual assistant created by Microsoft Inc.")
     }
 }
 
+function darkMode() {
+    document.getElementById('mydivheader').style.backgroundColor = 'black';
+    document.getElementById('usersettingsheader').style.backgroundColor = 'black';
+    document.getElementById('browserheader').style.backgroundColor = 'black';
+    document.getElementById('textappheader').style.backgroundColor = 'black';
+    document.getElementById('scriptstoreheader').style.backgroundColor = 'black';
+    document.getElementById('settingsheader').style.backgroundColor = 'black';
+    document.getElementById('filesappheader').style.backgroundColor = 'black';
+    document.getElementById('backgroundsettingsheader').style.backgroundColor = 'black';
+}
+
+function lightMode() {
+    document.getElementById('mydivheader').style.backgroundColor = 'orangered';
+    document.getElementById('usersettingsheader').style.backgroundColor = 'orangered';
+    document.getElementById('browserheader').style.backgroundColor = 'orangered';
+    document.getElementById('textappheader').style.backgroundColor = 'orangered';
+    document.getElementById('scriptstoreheader').style.backgroundColor = 'orangered';
+    document.getElementById('settingsheader').style.backgroundColor = 'orangered';
+    document.getElementById('filesappheader').style.backgroundColor = 'orangered';
+}
+
+function save() {
+    var filetext = document.getElementById('text-box').textContent;
+    var textfileicon = '<input type="image" id="textfilename" src="https://img11.androidappsapk.co/300/2/2/7/com.JellyBeanUser.apk.app.editor.png" style="width:50px; height=50px" onclick="openFile()"/>';
+    document.getElementById('filesbox').innerHTML += textfileicon;
+}
+function openFile() {
+    document.getElementById('textapp').style.display = 'inline';
+    document.getElementById('text-box').value = filetext;
+}
+
+function startDictation() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+        var recognition = new webkitSpeechRecognition();
+
+        recognition.continuous = false;
+        recognition.interimResults = false;
+
+        recognition.lang = "en-US";
+        recognition.start();
+
+        recognition.onresult = function (e) {
+            document.getElementById('input').value
+                = e.results[0][0].transcript;
+            recognition.stop();
+            document.getElementById('input').submit();
+        };
+
+        recognition.onerror = function (e) {
+            recognition.stop();
+        }
+
+    }
+}
