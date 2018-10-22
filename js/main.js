9eddf71fee6189865b4df28838a4535ca35c30d1
TenZe Inc <33192847+TenZeInc@users.noreply.github.com>
2018-10-22 15:41:12 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -359,23 +359,6 @@ function dragElement(elmnt) {
 
 }
 
-
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-        h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-    if (i < 10) { i = "0" + i };
-    return i;
-}
-
 var input = document.getElementById('input');
 var form = document.getElementById('commands');
 
@@ -515,4 +498,10 @@ function startDictation() {
         }
 
     }
+}
+
+function login() {
+    var username = document.getElementById('username').value;
+
+    document.getElementById('userloginname').innerHTML = username;
 }
