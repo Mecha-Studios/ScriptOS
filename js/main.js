32a53f9d9f916063fb5fe9201a4e52ff51d2c374
TenZe Inc <33192847+TenZeInc@users.noreply.github.com>
2019-02-22 13:09:35 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 ﻿function dragElement(id) {
     var elmnt = document.getElementById(id);
-    if(elmnt === null) {
+    if (elmnt === null) {
         console.warn('Element does not exist, id="' + id + '"');
         return;
     }
@@ -46,131 +46,6 @@ dragElement("filesapp");
 dragElement("apps");
 dragElement("clockapp");
 
-var input = document.getElementById('input');
-var form = document.getElementById('commands');
-
-function processForm(e) {
-
-    if (e.preventDefault) e.preventDefault();
-    start(input.value, out)
-    return false;
-
-}
-
-if (form.attachEvent) {
-    form.attachEvent("submit", processForm)
-}
-else {
-    form.addEventListener("submit", processForm)
-}
-
-var output = document.getElementById('output');
-
-var out = function (outtext) {
-
-    console.log(outtext);
-    var newPara = document.createElement('p');
-    var paraText = document.createTextNode(outtext);
-    newPara.appendChild(paraText);
-    output.appendChild(newPara);
-
-}
-
-
-function start(invar, outvar) {
-    if (invar == "hi") {
-        outvar("Hey!");
-    }
-    if (invar == "hey") {
-        outvar("Hi!");
-    }
-    if (invar == "hello") {
-        outvar("Hi there!")
-    }
-    if (invar == "how are you") {
-        outvar("I'm doing pretty good, you?");
-    }
-    if (invar == "do you have voice controll") {
-        outvar("Yes!");
-    }
-    if (invar == "whats your name") {
-        outvar("My name is Zec. I am your digital assistant.");
-    }
-    if (invar == "who is Cortana") {
-        outvar("Cortana is a virtual assistant created by Microsoft Inc.");
-    }
-    if (invar == "fuck you") {
-        outvar("That's not very nice!");
-    }
-    if (invar == "launch settings") {
-        outvar("Sure thing!");
-        document.getElementById('settings').style.display = 'inline';
-    }
-    if (invar == "launch browser") {
-        outvar("Okay!");
-        document.getElementById('browser').style.display = 'inline';
-    }
-    if (invar == "clear") {
-        document.getElementById('output').innerHTML = '';
-    }
-    if (invar == "goodbye") {
-        outvar("Goodbye!");
-    }
-    if (invar == false) {
-        outvar("Sorry, I don't understand");
-    }
-}
-
-function save() {
-    var filetext = document.getElementById('text-box').textContent;
-    var textfileicon = '<input type="image" id="textfilename" src="https://img11.androidappsapk.co/300/2/2/7/com.JellyBeanUser.apk.app.editor.png" style="width:50px; height=50px" onclick="openFile()"/>';
-    document.getElementById('filesbox').innerHTML += textfileicon;
-}
-function openFile() {
-    document.getElementById('textapp').style.display = 'inline';
-    document.getElementById('text-box').value = filetext;
-}
-
-function startDictation() {
-
-    if (window.hasOwnProperty('webkitSpeechRecognition')) {
-
-        var recognition = new webkitSpeechRecognition();
-
-        recognition.continuous = false;
-        recognition.interimResults = false;
-
-        recognition.lang = "en-US";
-        recognition.start();
-
-        recognition.onresult = function (e) {
-            document.getElementById('input').value
-                = e.results[0][0].transcript;
-            recognition.stop();
-            document.getElementById('input').submit();
-        };
-
-        recognition.onerror = function (e) {
-            recognition.stop();
-        }
-
-    }
-}
-
-
-function openExampleFolder() {
-    document.getElementById('filesbox').innerHTML = '<input type="image" title="Chicken.html" src="http://icons.iconarchive.com/icons/pelfusion/flat-file-type/256/html-icon.png" onclick="openChickenHtml()" style="width: 50px; height: 50px;"/>'
-}
-
-function openChickenHtml() {
-    document.getElementById('htmlview').style.display = 'inline';
-    document.getElementById('htmlviewer').src = 'Chicken.html';
-}
-
-function openScriptOSFolder() {
-    document.getElementById('filesbox').innerHTML = '<input type="image" name="images/Script OS Logo.png" title="Script OS Logo.png" src="images/Script OS Logo.png" onclick="openScriptOSImage()" style="width: 50px; height: 50px;"/>'
-}
-
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -184,4 +59,3 @@ function checkTime(i) {
     if (i < 10) { i = "0" + i };
     return i;
 }
-
