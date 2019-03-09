dfa2983a6d9451daae5bae68f628c627daf86b7d
TenZe Inc <33192847+TenZeInc@users.noreply.github.com>
2019-03-08 21:37:07 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-﻿function dragWindow(id) {
-    var elmnt = document.getElementById(id);
-    if (id === null) {
-        console.warn('Element does not exist, id="' + id + '"');
-        return;
-    }
-
+﻿function dragWindow(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt + "header")) {
-        document.getElementById(elmnt + "header").onmousedown = dragMouseDown;
+    if (document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
     } else {
         elmnt.onmousedown = dragMouseDown;
     }
 
     function dragMouseDown(e) {
         e = e || window.event;
+        e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
@@ -22,6 +17,7 @@
 
     function elementDrag(e) {
         e = e || window.event;
+        e.preventDefault();
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
@@ -36,13 +32,15 @@
     }
 }
 
-dragWindow("mydiv");
-dragWindow("settings");
-dragWindow("backgroundsettings");
-dragWindow("usersettings");
-dragWindow("browser");
-dragWindow("filesapp");
-dragWindow("apps");
+dragWindow(document.getElementById("mydiv"));
+dragWindow(document.getElementById("settings"));
+dragWindow(document.getElementById("backgroundsettings"));
+dragWindow(document.getElementById("usersettings"));
+dragWindow(document.getElementById("browser"));
+dragWindow(document.getElementById("filesapp"));
+dragWindow(document.getElementById("apps"));
+dragWindow(document.getElementById("textapp"));
+dragWindow(document.getElementById("htmlview"));
 
 function startTime() {
     var today = new Date();
@@ -57,3 +55,23 @@ function checkTime(i) {
     if (i < 10) { i = "0" + i };
     return i;
 }
+
+var input = document.getElementById("fileopen");
+var output = document.getElementById("text-box");
+
+
+input.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+        var myFile = this.files[0];
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function (e) {
+            output.textContent = e.target.result;
+        });
+
+        reader.readAsBinaryString(myFile);
+    }
+});
+
+var htmlinput = document.getElementById("openhtml").value;
+document.getElementById('htmlviewer').src = htmlinput;
